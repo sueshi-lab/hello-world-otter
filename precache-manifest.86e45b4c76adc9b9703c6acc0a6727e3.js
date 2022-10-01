e8a8d89cf1dbaa993af2e4a2e90320243fb678ea
sueshi-lab <phanpaporn.zcx@gmail.com>
2022-10-01 22:56:33 +0700

    Updates

---
+++ precache-manifest.86e45b4c76adc9b9703c6acc0a6727e3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "534883eb2bb2ab82e3dc",
+    "url": "/hello-world-otter/static/css/main.f3e0e6c8.chunk.css"
+  },
+  {
+    "revision": "534883eb2bb2ab82e3dc",
+    "url": "/hello-world-otter/static/js/main.534883eb.chunk.js"
+  },
+  {
+    "revision": "3cda334bc2ffd78cf805",
+    "url": "/hello-world-otter/static/js/1.3cda334b.chunk.js"
+  },
+  {
+    "revision": "51b04d71e92d6fd1d5fa",
+    "url": "/hello-world-otter/static/js/runtime~main.51b04d71.js"
+  },
+  {
+    "revision": "2363d65e9cef59b512480792bf8275e7",
+    "url": "/hello-world-otter/index.html"
+  }
+];
